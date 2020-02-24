e4e76722421da971f69e8a8c268b3724d7b0f1b2
TheOrangePuff <danielvdp56@gmail.com>
2020-02-24 14:52:00 +1030

    Updated README

--- .config/polybar/startup.sh
+++ .config/polybar/startup.sh
@@ -4,7 +4,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+HOSTNAME=$(cat /etc/hostname)
+
 # Start polybar on all monitors
 for m in $(polybar -m | cut -d":" -f1); do
+  if [ $HOSTNAME = 'Daniels-Laptop' ]; then
+    MONITOR=$m polybar -r laptop &
+  else
     MONITOR=$m polybar -r mainbar &
+  fi
 done
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # My dotfiles
+
+# Install
+To install on a new system follow the instructions in this [Atlassian](https://www.atlassian.com/git/tutorials/dotfiles) article.
