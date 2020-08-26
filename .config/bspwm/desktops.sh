2ddf2ee93827e914f3bb7be393448c4f66125b11
TheOrangePuff <danielvdp56@gmail.com>
2020-08-26 09:58:54 +0930

    Changed work laptop config

--- .config/bspwm/desktops.sh
+++ .config/bspwm/desktops.sh
@@ -4,10 +4,11 @@
 if [ $HOSTNAME = "Daniels-Laptop" ]; then
 	# if only one monitor
     if [ $(xrandr | grep " connected " | awk '{ print$1 }' | wc -l) = 1 ]; then
-        bspc monitor eDP-1 -d "Browser 1" Terminal Misc VM Terminal "Browser 2" Misc
+        bspc monitor eDP-1 -d "Browser 1" Terminal Misc Terminal "Browser 2" VM Terminal Misc Misc
     else
-        bspc monitor HDMI-2 -d "Browser 1" Terminal Misc VM
-	    bspc monitor eDP-1 -d Terminal "Browser 2" Misc
+        bspc monitor DP-1 -d "Browser 1" Terminal Misc
+        bspc monitor HDMI-2 -d Terminal "Browser 2" VM
+	    bspc monitor eDP-1 -d Terminal Misc Misc
     fi
 else
 	bspc monitor DVI-D-0 -d Chat Misc
@@ -15,4 +16,3 @@ else
 	bspc monitor DP-0 -d Terminal "Browser Secondary" Steam
 fi
 
-###
