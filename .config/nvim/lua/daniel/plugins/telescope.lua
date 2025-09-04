e12fdcf44abec5124d912856b067a9a1084ae258
Daniel van der Ploeg <daniel.vanderploeg@aligent.com.au>
2025-09-04 17:14:25 +0930

    fix: issue with .git being to vague

--- .config/nvim/lua/daniel/plugins/telescope.lua
+++ .config/nvim/lua/daniel/plugins/telescope.lua
@@ -6,8 +6,13 @@ return {
         require("telescope").setup({
             defaults = {
                 file_ignore_patterns = {
-                    "node_modules",
-                    ".git",
+                    "node_modules/",
+                    "%.git/",
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
                 },
             },
         })
