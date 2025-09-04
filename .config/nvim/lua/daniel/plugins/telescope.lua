70cd785f955846f3a05cf172f10bf8b46516429a
Daniel van der Ploeg <daniel.vanderploeg@aligent.com.au>
2025-09-04 13:05:37 +0930

    feat: telescope add ignore patterns

--- .config/nvim/lua/daniel/plugins/telescope.lua
+++ .config/nvim/lua/daniel/plugins/telescope.lua
@@ -3,18 +3,14 @@ return {
     tag = "0.1.6",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-        require("telescope").setup()
-        -- require("telescope").setup({
-        --     extensions = {
-        --         fzf = {
-        --             fuzzy = true,                    -- false will only do exact matching
-        --             override_generic_sorter = true,  -- override the generic sorter
-        --             override_file_sorter = true,     -- override the file sorter
-        --             case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-        --             -- the default case_mode is "smart_case"
-        --         }
-        --     }
-        -- }).load_extension('fzf')
+        require("telescope").setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    ".git",
+                },
+            },
+        })
 
         local keymap = vim.keymap
 
